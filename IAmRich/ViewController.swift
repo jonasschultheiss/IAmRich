dbd0bfa22e1b00b7fd760d46f81cd547fdd0bbb4
Jonas Raphael Schultheiss <dev.prsm@gmail.com>
2018-09-14 15:31:00 +0200

    Initial Commit

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Jonas Raphael Schultheiss on 14.09.18.
+//  Copyright © 2018 Jonas Raphael Schultheiss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
